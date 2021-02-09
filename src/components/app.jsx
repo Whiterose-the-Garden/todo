41f44e4a0f2411fcb71875acfec0009630ac7e9b
sm-hwang <steven.m.hwang@gmail.com>
2021-02-09 11:02:43 -0700

    initial commit, start script working

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h1>Hello, electron!</h1>
+    <p>Let's start building your awesome desktop app with electron and React!</p>
+  </div>
+)
+
+export default App
